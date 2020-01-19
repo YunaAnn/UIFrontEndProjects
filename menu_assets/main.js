c66b8c3f4b14d83a61d6fabd4150792532a9ac18
YunaAnn <ann.grzywnowicz@gmail.com>
2020-01-19 17:00:06 +0100

    menu done

---
+++ menu_assets/main.js
@@ -0,0 +1,32 @@
+/***LOADING***/
+var time;
+
+function loading()
+{
+  time = setTimeout(showPage, 2000);
+}
+
+function showPage()
+{
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("page").style.display = "block";
+}
+
+/***MENU***/
+
+var i=0;
+function expand()
+{
+  if(i==0)
+  {
+   document.getElementById("menu").style.transform="scale(3)"; 
+   document.getElementById("open").style.transform="rotate(45deg)"; 
+   i=1;
+  }
+  else
+  {
+   document.getElementById("menu").style.transform="scale(0)"; 
+   document.getElementById("open").style.transform="rotate(0deg)"; 
+   i=0;
+  }
+}
